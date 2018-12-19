fef953eec866ceb61bc6088b6b142ab7c7e9a3f0
Jaja Pabatao <bcpabatao.halcyondigital@gmail.com>
2018-12-19 13:28:26 +0800

    make it better

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://jajap:jajap123@ds135974.mlab.com:35974/mern-shopping'
+};
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path')
+
+const items = require('./routes/api/items')
+
+const app = express();
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose.connect(db)
+  .then(() => console.log('MongoDb Connected...')).catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items)
+
+// Serve static asset if in production 
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
