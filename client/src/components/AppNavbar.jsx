f9c9d07c5c1617750f040af01b13655c7b4ec458
Jaja Pabatao <bcpabatao.halcyondigital@gmail.com>
2018-12-24 10:24:34 +0800

    minor fixes

--- client/src/components/AppNavbar.jsx
+++ client/src/components/AppNavbar.jsx
@@ -22,24 +22,25 @@ class AppNavbar extends Component {
   };
 
   render() {
-      return(
-    <div>
-      <Navbar color="dark" dark expand="sm" className="mb-5">
-        <Container>
-          <NavbarBrand href="/">ShoppingList</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="https://github.com/jajapabatao">Github</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
-    </div>
-      );
-    
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/jajapabatao">
+                    Github
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
   }
 }
 
--- client/src/components/ItemModal.jsx
+++ client/src/components/ItemModal.jsx
@@ -1,88 +1,87 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
-    Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    Form,
-    FormGroup,
-    Label,
-    Input
-} from 'reactstrap'
-import { connect } from 'react-redux'
-import { addItem } from '../actions/itemActions.jsx'
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+import { connect } from "react-redux";
+import { addItem } from "../actions/itemActions.jsx";
 
 class ItemModal extends Component {
-    state = {
-        modal: false,
-        name: ''
-    }
+  state = {
+    modal: false,
+    name: ""
+  };
 
-    toggle = () => {
-        this.setState({
-            modal: !this.state.modal
-        })
-    }
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
 
-    onChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    onSubmit = e => {
-        e.preventDefault();
-        const newItem = {
-            name: this.state.name
-        }
+  onSubmit = e => {
+    e.preventDefault();
+    const newItem = {
+      name: this.state.name
+    };
 
-        // Add item via addItem action
-        this.props.addItem(newItem)
+    // Add item via addItem action
+    this.props.addItem(newItem);
 
-        // close modal
-        this.toggle()
-    }
+    // close modal
+    this.toggle();
+  };
 
-    render() {
-        return (
-            <div>
+  render() {
+    return (
+      <div>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={this.toggle}
+        >
+          Add Item
+        </Button>
 
-                <Button
-                    color="dark"
-                    style={{ marginBottom: '2rem' }}
-                    onClick={this.toggle}
-                >Add Item</Button>
-
-                <Modal isOpen={this.state.modal} toggle={this.toggle}>
-                    <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
-                    <ModalBody>
-                        <form onSubmit={this.onSubmit}>
-                            <FormGroup>
-                                <Label for="item">Item</Label>
-                                <Input
-                                    type="text"
-                                    name="name"
-                                    id="name"
-                                    placeholder="Add shopping items"
-                                    onChange={this.onChange}
-                                />
-                                <Button
-                                    color="dark"
-                                    style={{ marginTop: '2rem' }}
-                                    block>
-                                    Add Item
-                                </Button>
-                            </FormGroup>
-                        </form>
-                    </ModalBody>
-                </Modal>
-
-            </div>
-        )
-    }
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
+          <ModalBody>
+            <form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="item">Item</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Add shopping items"
+                  onChange={this.onChange}
+                />
+                <Button color="dark" style={{ marginTop: "2rem" }} block>
+                  Add Item
+                </Button>
+              </FormGroup>
+            </form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
 }
 const mapStateToProps = state => ({
-    item: state.item
-})
+  item: state.item
+});
 
-export default connect(mapStateToProps, { addItem })(ItemModal)
+export default connect(
+  mapStateToProps,
+  { addItem }
+)(ItemModal);
